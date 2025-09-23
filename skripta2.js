b606f26f844f5da864d35a8618e1c4b7a0a0777f
laura <150249389+lauraduic@users.noreply.github.com>
2025-09-23 09:17:22 +0200

    Add files via upload

---
+++ skripta2.js
@@ -0,0 +1,18 @@
+let ime = "laura"
+let prezime = "duić"
+let a = 2;
+let b = 5;
+let c= a + b;
+
+const pi = 3.14;
+const oib = 12345678991;
+
+console.log(ime);
+console.log(prezime);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(pi);
+console.log(oib);
+
+document.getElementById("ispis").innerHTML = ime;
