ee8fab814e6b186a88889e243c0eb3122972115b
laura <150249389+lauraduic@users.noreply.github.com>
2025-09-30 09:34:56 +0200

    Add files via upload

---
+++ skripta3.js
@@ -0,0 +1,21 @@
+let a = 83;
+let b = 3.14;
+
+let boja = "žuta";
+let prezime = "Horvat";
+
+let x = true;
+let y = false;
+
+const auti = ["BMW","Audi","Mercedes"];
+
+const osoba = {ime:"Ivo", prezime:"Ivić"};
+
+const date = new Date("2025-09-30");
+
+console.log(a);
+console.log(b);
+console.log(boja);
+console.log(prezime);
+console.log(x);
+console.log(y);
