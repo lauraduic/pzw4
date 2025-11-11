a4001f91d6544e6bcbaac20ad47620d7addf0fe9
laura <150249389+lauraduic@users.noreply.github.com>
2025-11-11 09:32:27 +0100

    Add files via upload

---
+++ skripta7.js
@@ -0,0 +1,11 @@
+function mojafunkcija(){
+document.getElementById("crveni").style.backgroundColor="blue";
+}
+
+function mojafunkcijaGUMB(){
+document.getElementById("crveni").textContent="pozdrav hakeri";
+}
+
+function mojafunkcija2(){
+    document.getElementById("crveni").style.backgroundColor="yellow","green";
+}
